f535b92a4aee58edf31fc019a935d059bdaacb9e
Ian Wu <ian.wu@c3.ai>
2023-07-18 17:15:48 +0800

    GENAI-1384: Create notebooks showcasing DPO usage, create fork of DPO repo with code modified to work in C3 Jupyter

--- train.py
+++ train.py
@@ -5,28 +5,31 @@ import transformers
 from utils import get_local_dir, get_local_run_dir, disable_dropout, init_distributed
 import os
 import hydra
+import torch.distributed as dist
 import torch.multiprocessing as mp
+mp.set_start_method('spawn', force=True)
 from omegaconf import OmegaConf, DictConfig
 import trainers
 import wandb
 import json
 import socket
 from typing import Optional, Set
-import resource
 
 
 OmegaConf.register_new_resolver("get_local_run_dir", lambda exp_name, local_dirs: get_local_run_dir(exp_name, local_dirs))
 
 
-def worker_main(rank: int, world_size: int, config: DictConfig, policy: nn.Module, reference_model: Optional[nn.Module] = None):
+def worker_main(rank: int, world_size: int, config: DictConfig):
     """Main function for each worker process (may be only 1 for BasicTrainer/TensorParallelTrainer)."""
+    print("work it!")
+    
     if 'FSDP' in config.trainer:
         init_distributed(rank, world_size, port=config.fsdp_port)
     
     if config.debug:
         wandb.init = lambda *args, **kwargs: None
         wandb.log = lambda *args, **kwargs: None
-
+        
     if rank == 0 and config.wandb.enabled:
         os.environ['WANDB_CACHE_DIR'] = get_local_dir(config.local_dirs)
         wandb.init(
@@ -36,7 +39,35 @@ def worker_main(rank: int, world_size: int, config: DictConfig, policy: nn.Modul
             dir=get_local_dir(config.local_dirs),
             name=config.exp_name,
         )
+    
+    print('building policy')
+    model_kwargs = {'device_map': 'balanced'} if config.trainer == 'BasicTrainer' else {}
+    policy_dtype = getattr(torch, config.model.policy_dtype)
+    policy = transformers.AutoModelForCausalLM.from_pretrained(
+        config.model.name_or_path, cache_dir=get_local_dir(config.local_dirs), low_cpu_mem_usage=True, torch_dtype=policy_dtype, **model_kwargs)
+    disable_dropout(policy)
+    #policy.to(rank)
 
+    if config.loss.name == 'dpo':
+        print('building reference model')
+        reference_model_dtype = getattr(torch, config.model.reference_dtype)
+        reference_model = transformers.AutoModelForCausalLM.from_pretrained(
+            config.model.name_or_path, cache_dir=get_local_dir(config.local_dirs), low_cpu_mem_usage=True, torch_dtype=reference_model_dtype, **model_kwargs)
+        disable_dropout(reference_model)
+        #reference_model.to(rank)
+    else:
+        reference_model = None
+
+    if config.model.archive is not None:
+        state_dict = torch.load(config.model.archive, map_location='cpu')
+        step, metrics = state_dict['step_idx'], state_dict['metrics']
+        print(f'loading pre-trained weights at step {step} from {config.model.archive} with metrics {json.dumps(metrics, indent=2)}')
+        policy.load_state_dict(state_dict['state'])
+        if config.loss.name == 'dpo':
+            reference_model.load_state_dict(state_dict['state'])
+        print('loaded pre-trained weights')
+    
+    print("rank: {}. Policy: {}".format(rank, policy))
     TrainerClass = getattr(trainers, config.trainer)
     print(f'Creating trainer on process {rank} with world size {world_size}')
     trainer = TrainerClass(policy, config, config.seed, config.local_run_dir, reference_model=reference_model, rank=rank, world_size=world_size)
@@ -72,41 +103,14 @@ def main(config: DictConfig):
     print('=' * 80)
  
     os.environ['XDG_CACHE_HOME'] = get_local_dir(config.local_dirs)
-    print('building policy')
-    model_kwargs = {'device_map': 'balanced'} if config.trainer == 'BasicTrainer' else {}
-    policy_dtype = getattr(torch, config.model.policy_dtype)
-    policy = transformers.AutoModelForCausalLM.from_pretrained(
-        config.model.name_or_path, cache_dir=get_local_dir(config.local_dirs), low_cpu_mem_usage=True, torch_dtype=policy_dtype, **model_kwargs)
-    disable_dropout(policy)
-
-    if config.loss.name == 'dpo':
-        print('building reference model')
-        reference_model_dtype = getattr(torch, config.model.reference_dtype)
-        reference_model = transformers.AutoModelForCausalLM.from_pretrained(
-            config.model.name_or_path, cache_dir=get_local_dir(config.local_dirs), low_cpu_mem_usage=True, torch_dtype=reference_model_dtype, **model_kwargs)
-        disable_dropout(reference_model)
-    else:
-        reference_model = None
-
-    if config.model.archive is not None:
-        state_dict = torch.load(config.model.archive, map_location='cpu')
-        step, metrics = state_dict['step_idx'], state_dict['metrics']
-        print(f'loading pre-trained weights at step {step} from {config.model.archive} with metrics {json.dumps(metrics, indent=2)}')
-        policy.load_state_dict(state_dict['state'])
-        if config.loss.name == 'dpo':
-            reference_model.load_state_dict(state_dict['state'])
-        print('loaded pre-trained weights')
     
     if 'FSDP' in config.trainer:
         world_size = torch.cuda.device_count()
         print('starting', world_size, 'processes for FSDP training')
-        soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
-        resource.setrlimit(resource.RLIMIT_NOFILE, (hard, hard))
-        print(f'setting RLIMIT_NOFILE soft limit to {hard} from {soft}')
-        mp.spawn(worker_main, nprocs=world_size, args=(world_size, config, policy, reference_model), join=True)
+        mp.spawn(worker_main, nprocs=world_size, args=(world_size, config), join=True)
     else:
         print('starting single-process worker')
-        worker_main(0, 1, config, policy, reference_model)
+        worker_main(0, 1, config)
 
 
 if __name__ == '__main__':
